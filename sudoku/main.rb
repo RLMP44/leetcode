fed1f2e7c4f381f48b4b6b43be665f9466448a4b
Rachael Momber <rlmomber@gmail.com>
2024-02-29 20:28:01 +0900

    added RSpec and got row validation working

--- sudoku/main.rb
+++ sudoku/main.rb
@@ -1,11 +1,12 @@
 def is_valid_sudoku(board)
   # set column []
   # set block []
-  9.times do
+  # 9.times do
     # choose column index number, starting with 0
     # set block index number, starting with 0
     board.each_with_index do |row, index|
       # check row for 1 - 9, return true or false
+      puts(valid_row?(row))
       # use set index number to find column and add to column array
       # increment index number for next column
 
@@ -13,19 +14,12 @@ def is_valid_sudoku(board)
         # get
       # reset column array
     end
-  end
+  # end
 end
 
-board =
-  [['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-   ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-   ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-   ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-   ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-   ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-   ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-   ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-   ['.', '.', '.', '.', '.', '.', '.', '.', '.']]
+def valid_row?(row)
+  row.sort == ('1'..'9').to_a
+end
 
 board2 =
   [['5', '3', '.', '.', '7', '.', '.', '.', '.'],
@@ -49,13 +43,17 @@ board3 =
    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
    ['.', '.', '.', '.', '8', '.', '.', '7', '9']]
 
-  # ---------------------- RULES ------------------------- #
-  # Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+# --------------------- TESTING ------------------------ #
+puts(is_valid_sudoku(board2))
 
-  # Each row must contain the digits 1-9 without repetition.
-  # Each column must contain the digits 1-9 without repetition.
-  # Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
-  # Note:
+# ---------------------- RULES ------------------------- #
+# Determine if a 9 x 9 Sudoku board is valid.
+# Only the filled cells need to be validated according to the following rules:
 
-  # A Sudoku board (partially filled) could be valid but is not necessarily solvable.
-  # Only the filled cells need to be validated according to the mentioned rules.
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
