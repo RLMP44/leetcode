5cb58b1d0c924b2e38003edecf0d9f3421958c7f
Rachael Momber <rlmomber@gmail.com>
2024-02-29 22:13:56 +0900

    main function successfully checks if rows and columns are valid. just missing block validation

--- sudoku/main.rb
+++ sudoku/main.rb
@@ -1,26 +1,33 @@
 def is_valid_sudoku(board)
-  # set column []
+  column = []
   # set block []
-  # 9.times do
-    # choose column index number, starting with 0
-    # set block index number, starting with 0
-    board.each_with_index do |row, index|
-      # check row for 1 - 9, return true or false
-      valid_row?(row)
-      # use set index number to find column and add to column array
-      # increment index number for next column
-
-      # three times do,
-        # get
-      # reset column array
+  # set column number
+  all_cols_valid = true
+  all_rows_valid = true
+  (0..8).to_a.each do |col|
+    # go through each row and get number at col index
+    (0..8).to_a.each do |row|
+      column << board[row][col]
     end
-  # end
+    all_cols_valid = false unless valid_line?(column)
+    column = []
+  end
+  board.each_with_index do |row, index|
+    # check row for 1 - 9, return true or false
+    all_rows_valid = false unless valid_line?(row)
+    # set block index number, starting with 0
+    # use set index number to find column and add to column array
+    # three times do,
+    # get
+    # reset column array
+  end
+  all_cols_valid && all_rows_valid
 end
 
-def valid_row?(row)
+def valid_line?(line)
   repeat = false
-  row.each do |num|
-    repeat = true if num != '.' && row.count(num) > 1
+  line.each do |num|
+    repeat = true if num != '.' && line.count(num) > 1
   end
   repeat ? false : true
 end
@@ -40,19 +47,19 @@ end
 #    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
 #    ['.', '.', '.', '.', '8', '.', '.', '7', '9']]
 
-# board3 =
-#   [['8', '3', '.', '.', '7', '.', '.', '.', '.'],
-#    ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-#    ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-#    ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-#    ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-#    ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-#    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-#    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-#    ['.', '.', '.', '.', '8', '.', '.', '7', '9']]
+board3 =
+  [['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+   ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+   ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+   ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+   ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+   ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+   ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+   ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+   ['.', '.', '.', '.', '8', '.', '.', '7', '9']]
 
 # --------------------- TESTING ------------------------ #
-# puts(is_valid_sudoku(board2))
+puts(is_valid_sudoku(board3))
 
 # ---------------------- RULES ------------------------- #
 # Determine if a 9 x 9 Sudoku board is valid.
--- sudoku/spec/is_valid_sudoku_spec.rb
+++ sudoku/spec/is_valid_sudoku_spec.rb
@@ -33,18 +33,26 @@ BOARD3 =
    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
    ['.', '.', '.', '.', '8', '.', '.', '7', '9']]
 
+COLUMN = [BOARD2[0][0], BOARD2[0][1], BOARD2[0][2], BOARD2[0][3], BOARD2[0][4], BOARD2[0][5], BOARD2[0][6], BOARD2[0][7], BOARD2[0][8]]
+COLUMN2 = [BOARD3[0][0], BOARD3[0][1], BOARD3[0][2], BOARD3[0][3], BOARD3[0][4], BOARD3[0][5], BOARD3[0][6], BOARD3[0][7], BOARD3[0][8]]
 
-describe '#valid_row?' do
+
+describe '#valid_line?' do
   it 'should return true if it has no repeated numbers' do
-    expect(valid_row?(BOARD[0])).to eq(true)
-    expect(valid_row?(BOARD2[0])).to eq(true)
-    expect(valid_row?(BOARD[1])).to eq(false)
+    expect(valid_line?(BOARD[0])).to eq(true)
+    expect(valid_line?(BOARD2[0])).to eq(true)
+    expect(valid_line?(BOARD[1])).to eq(false)
+    expect(valid_line?(COLUMN)).to eq(true)
+    expect(valid_line?(COLUMN2)).to eq(true)
   end
 end
 
 describe '#is_valid_sudoku' do
   it 'should return true if sudoku board has no repeat numbers in row, column, or block' do
+    expect(is_valid_sudoku(BOARD)).to eq(false)
     expect(is_valid_sudoku(BOARD2)).to eq(true)
+    expect(is_valid_sudoku(BOARD3)).to eq(false)
+
   end
 end
 
