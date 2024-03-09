276a907ffbd876db9d1bdfdde7bf4ff86b80e075
Rachael Momber <rlmomber@gmail.com>
2024-03-09 18:17:13 +0900

    passing all tests on leetcode, but failing rspec

--- sudoku/main.rb
+++ sudoku/main.rb
@@ -1,48 +1,27 @@
 def is_valid_sudoku(board)
-  @column = []
-  @is_valid = true
-  @block = []
-  board.each_with_index do |row, index|
-    @is_valid = false unless valid_line?(row)
-    handle_column(board, index)
-    handle_block(board, index)
-  end
+  set_vars
+  handle_row_and_col(board)
+  handle_block(board)
   @is_valid
 end
 
-def handle_column(board, index)
-  make_column(board, index)
-  @is_valid = false unless valid_line?(@column)
+def set_vars
   @column = []
+  @is_valid = true
+  @block = []
+  @block_column = 0
+  @iteration = 1
 end
 
-def make_column(board, row)
-  # receive a row index, assign column number using 0-8, swap order to get column instead of row
-  (0..8).to_a.each do |col|
-    @column << board[col][row]
+def handle_row_and_col(board)
+  board.each_with_index do |row, index|
+    validity_set(row)
+    handle_column(board, index)
   end
 end
 
-def handle_block(board, index)
-  make_block(board, index)
-  # validate block only on every 3rd iteration
-  return unless [2, 5, 8].include?(index)
-
-  @is_valid = false unless valid_line?(@block.flatten!)
-  @block = []
-end
-
-def make_block(board, index)
-  inc_amount = increment(index)
-  @block << [board[index][0 + inc_amount], board[index][1 + inc_amount], board[index][2 + inc_amount]]
-end
-
-def increment(index)
-  # method for blocks, must only increment after every 3rd iteration
-  inc_amount = 0
-  inc_amount = 3 if index == 3
-  inc_amount = 6 if index == 6
-  inc_amount
+def validity_set(input)
+  @is_valid = false unless valid_line?(input)
 end
 
 def valid_line?(line)
@@ -54,9 +33,42 @@ def valid_line?(line)
   repeat ? false : true
 end
 
-# def finished_line?(line)
-#   line.sort == ('1'..'9').to_a
-# end
+def handle_column(board, index)
+  make_column(board, index)
+  validity_set(@column)
+  @column = []
+end
+
+def make_column(board, row)
+  # receive a row index, assign column number using 0-8, swap order to get column instead of row
+  (0..8).to_a.each do |col|
+    @column << board[col][row]
+  end
+end
+
+def handle_block(board)
+  3.times do
+    new_block(board)
+    @block_column += 3
+    @increment = 0
+  end
+end
+
+@increment = 0
+def new_block(board)
+  9.times do
+    @block << board.flatten[(0 + @block_column + @increment)..(2 + @block_column + @increment)]
+    execute_block
+    @increment += 9
+  end
+end
+
+def execute_block
+  return unless @block.count == 3
+
+  validity_set(@block.flatten)
+  @block = []
+end
 
 # --------------------- TEST CASES ----------------------- #
 
@@ -109,7 +121,7 @@ board5 =
   [['.', '.', '.', '.', '.', '.', '.', '.', '.'],
    ['4', '.', '.', '.', '.', '.', '.', '.', '.'],
    ['.', '.', '.', '.', '.', '.', '6', '.', '.'],
-   ['.', '.', '.', '3', '8', '.', '.', '.', '.'],
+   ['.', '.', '.', '3', '8', '.', '.', '.', '6'],
    ['.', '5', '.', '.', '.', '6', '.', '.', '1'],
    ['8', '.', '.', '.', '.', '.', '.', '6', '.'],
    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
@@ -117,7 +129,7 @@ board5 =
    ['.', '.', '.', '6', '.', '.', '.', '.', '.']]
 
 # --------------------- TESTING ------------------------ #
-puts(is_valid_sudoku(board5))
+puts(is_valid_sudoku(board))
 
 # ---------------------- RULES ------------------------- #
 # Determine if a 9 x 9 Sudoku board is valid.
