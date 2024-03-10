627f81add6fd63150b42d47abb63f9fa430a795b
Rachael Momber <rlmomber@gmail.com>
2024-03-10 11:38:29 +0900

    complete initial setup and rspec

---
+++ two-sum/spec/two_sum_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../main.rb'
+
+describe '#two_sum' do
+  it 'should return an array of indices of two numbers that equal the target' do
+    expect(two_sum([2, 7, 11, 15], 9)).to eq([0, 1])
+    expect(two_sum([3, 2, 4], 6)).to eq([1, 2])
+    expect(two_sum([3, 3], 6)).to eq([0,1])
+  end
+end
