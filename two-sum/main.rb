ee81549ea0993fd4832e977e526ae26ea4424ef6
Rachael Momber <rlmomber@gmail.com>
2024-03-10 11:46:44 +0900

    tests passing but run time is too long

--- two-sum/main.rb
+++ two-sum/main.rb
@@ -2,5 +2,10 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-
+  indexes = nums.each_with_index.map do |num, index|
+    nums.each_with_index do |n, i|
+      return [index, i] if index != i && num + n == target
+    end
+  end
+  indexes
 end
