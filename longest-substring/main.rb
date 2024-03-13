67ce36a03683ba49b7a6bf22f4a53d155fbd6d11
Rachael Momber <rlmomber@gmail.com>
2024-03-13 21:15:10 +0900

    passing new test

--- longest-substring/main.rb
+++ longest-substring/main.rb
@@ -3,23 +3,21 @@
 def length_of_longest_substring(s)
   @checked = {}
   @longest = 0
-  @run = 0
-  s.chars.each_with_index do |letter, index|
-    @checked[letter] ? reset : handle_non_repeat(letter)
+  s.chars.each do |letter|
+    @checked[letter] ? reset(letter) : handle_non_repeat(letter)
   end
   return @longest
 end
 
-def reset
+def reset(letter)
   @checked = {}
-  @run = 0
-end
-
-def handle_non_repeat(letter)
-  @run += 1
-  @longest = @run if @run > @longest
   @checked[letter] = true
 end
 
+def handle_non_repeat(letter)
+  @checked[letter] = true
+  @longest = @checked.count if @checked.count > @longest
+end
 
-p(length_of_longest_substring("abcabcbb"))
+
+p(length_of_longest_substring("aab"))
