d416d5ae5d70cb896bae557aa9d739f790dade74
Rachael Momber <rlmomber@gmail.com>
2024-03-13 20:19:56 +0900

    initial setup

---
+++ longest-substring/spec/length_of_longest_substring_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../main.rb'
+
+describe '#length_of_longest_substring' do
+  it 'should return the length of the longest substring' do
+    expect(length_of_longest_substring("abcabcbb")).to eq(3)
+    expect(length_of_longest_substring("bbbbb")).to eq(1)
+    expect(length_of_longest_substring("pwwkew")).to eq(3)
+  end
+end
