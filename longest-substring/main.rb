43a61756d680e897f9997f1b3ba2f10d24855a52
Rachael Momber <rlmomber@gmail.com>
2024-03-13 20:37:22 +0900

    solve problem

--- longest-substring/main.rb
+++ longest-substring/main.rb
@@ -1,5 +1,25 @@
 # @param {String} s
 # @return {Integer}
 def length_of_longest_substring(s)
-
+  @checked = {}
+  @longest = 0
+  @run = 0
+  s.chars.each_with_index do |letter, index|
+    @checked[letter] ? reset : handle_non_repeat(letter)
+  end
+  return @longest
 end
+
+def reset
+  @checked = {}
+  @run = 0
+end
+
+def handle_non_repeat(letter)
+  @run += 1
+  @longest = @run if @run > @longest
+  @checked[letter] = true
+end
+
+
+p(length_of_longest_substring("abcabcbb"))
