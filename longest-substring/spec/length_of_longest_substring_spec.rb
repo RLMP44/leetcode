b0920eb02e55ef4f3002f156d3e6dac35bb6a801
Rachael Momber <rlmomber@gmail.com>
2024-03-13 20:39:19 +0900

    add new rspec test

--- longest-substring/spec/length_of_longest_substring_spec.rb
+++ longest-substring/spec/length_of_longest_substring_spec.rb
@@ -5,5 +5,6 @@ describe '#length_of_longest_substring' do
     expect(length_of_longest_substring("abcabcbb")).to eq(3)
     expect(length_of_longest_substring("bbbbb")).to eq(1)
     expect(length_of_longest_substring("pwwkew")).to eq(3)
+    expect(length_of_longest_substring("aab")).to eq(2)
   end
 end
