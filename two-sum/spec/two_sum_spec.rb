3c81343f4e41fee67965a19db5a9c287ab17f257
Rachael Momber <rlmomber@gmail.com>
2024-03-13 20:05:50 +0900

    solved again, passes all tests without timing out

--- two-sum/spec/two_sum_spec.rb
+++ two-sum/spec/two_sum_spec.rb
@@ -4,7 +4,7 @@ describe '#two_sum' do
   it 'should return an array of indices of two numbers that equal the target' do
     expect(two_sum([2, 7, 11, 15], 9)).to eq([0, 1])
     expect(two_sum([3, 2, 4], 6)).to eq([1, 2])
-    expect(two_sum([3, 3], 6)).to eq([0,1])
+    expect(two_sum([3, 3], 6)).to eq([0, 1])
     expect(two_sum([3, 2, 3], 6)).to eq([0, 2])
   end
 end
