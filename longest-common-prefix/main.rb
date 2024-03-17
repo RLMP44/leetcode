a57c0245a112a440e1384c4a0a6ed7cc0ce9204a
Rachael Momber <rlmomber@gmail.com>
2024-03-17 19:29:53 +0900

    refactor with comments

--- longest-common-prefix/main.rb
+++ longest-common-prefix/main.rb
@@ -1,47 +1,17 @@
 def longest_common_prefix(strs)
   @repeats = ''
-  @repeat_prefixes = []
-  @current_common = ''
-  return '' if strs.include?('')
-  return strs[0] unless strs.count > 1
-  return '' unless same_first_letter?(strs)
-
-  compare(strs)
-  @repeats = select_common_prefix
-end
-
-def compare(strs)
-  strs.each_with_index do |string, index|
-    strs.each_with_index do |str, i|
-      compare_word(string, str) if index != i
-      reset_current_common
+  strs.sort_by!(&:length)
+  @first_word = strs[0]
+  # iterate over length of shortest word
+  for num in (0..@first_word.length - 1)
+    # start at second word
+    strs[1..].each do |str|
+      return @repeats unless str[num] == @first_word[num]
     end
+    # use iteration number to add current letter
+    @repeats += @first_word[num]
   end
-end
-
-def same_first_letter?(strs)
-  letter = strs.first.chars.first
-  same = strs.select { |string| string.start_with?(letter) }
-  strs.count == same.count
-end
-
-def compare_word(string, str)
-  string.chars.each_with_index do |letter, index|
-    break unless string[index] == str[index]
-
-    @current_common += letter
-  end
-end
-
-def reset_current_common
-  @repeat_prefixes << @current_common
-  @current_common = ''
-end
-
-def select_common_prefix
-  @repeat_prefixes.sort_by!(&:length)
-  prefixes = @repeat_prefixes.reject { |prefix| prefix == '' }
-  prefixes[0] || ''
+  @repeats
 end
 
 p(longest_common_prefix(['reflower','flow','flight']))
