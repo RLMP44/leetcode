51ec3b9bf26784199bd3a8cc50ced292fd74a437
Rachael Momber <rlmomber@gmail.com>
2024-03-17 12:52:05 +0900

    passing 110 tests

--- longest-common-prefix/main.rb
+++ longest-common-prefix/main.rb
@@ -1,3 +1,49 @@
 def longest_common_prefix(strs)
+  @repeats = ''
+  @repeat_prefixes = []
+  @current_common = ''
+  return '' if strs.include?('')
+  return strs[0] unless strs.count > 1
+  return '' unless same_first_letter?(strs)
 
+  compare(strs)
+  @repeats = select_common_prefix
 end
+
+def compare(strs)
+  for string in strs
+    for str in strs
+      compare_word(string, str)
+      reset_current_common
+    end
+  end
+end
+
+def same_first_letter?(strs)
+  letter = strs.first.chars.first
+  same = strs.select { |string| string.start_with?(letter) }
+  strs.count == same.count
+end
+
+def compare_word(string, str)
+  return unless string != str
+
+  string.chars.each_with_index do |letter, index|
+    break unless string[index] == str[index]
+
+    @current_common += letter
+  end
+end
+
+def reset_current_common
+  @repeat_prefixes << @current_common
+  @current_common = ''
+end
+
+def select_common_prefix
+  @repeat_prefixes.sort_by!(&:length)
+  prefixes = @repeat_prefixes.reject { |prefix| prefix == '' }
+  prefixes[0] || ''
+end
+
+p(longest_common_prefix(['reflower','flow','flight']))
--- longest-common-prefix/spec/longest_common_prefix_spec.rb
+++ longest-common-prefix/spec/longest_common_prefix_spec.rb
@@ -2,7 +2,11 @@ require_relative '../main.rb'
 
 describe '#longest_common_prefix' do
   it 'should return the longest common prefix in an array' do
-    expect(longest_common_prefix(["flower","flow","flight"])).to eq("fl")
-    expect(longest_common_prefix(["dog","racecar","car"])).to eq("")
+    expect(longest_common_prefix(['flower','flow','flight'])).to eq('fl')
+    expect(longest_common_prefix(['dog','racecar','car'])).to eq('')
+    expect(longest_common_prefix([''])).to eq('')
+    expect(longest_common_prefix(['a'])).to eq('a')
+    expect(longest_common_prefix(['', ''])).to eq('')
+    expect(longest_common_prefix(['reflower','flow','flight'])).to eq('')
   end
 end
