34c768c6e3a7e3e353d587c548565dd11ccba60c
Rachael Momber <rlmomber@gmail.com>
2024-03-19 16:03:53 +0900

    overhaul all methods

--- longest-substring/main.rb
+++ longest-substring/main.rb
@@ -1,23 +1,48 @@
-# @param {String} s
-# @return {Integer}
 def length_of_longest_substring(s)
-  @checked = {}
-  @longest = 0
-  s.chars.each do |letter|
-    @checked[letter] ? reset(letter) : handle_non_repeat(letter)
+  @substring = 0
+  @list = ''
+  run(s.chars)
+end
+
+def run(s)
+  s.each_with_index do |character, index|
+    handle_repeat(character, index) if repeat?(character)
+    add_to_list(character)
+    update_substring
   end
-  return @longest
 end
 
-def reset(letter)
-  @checked = {}
-  @checked[letter] = true
+def update_substring
+  @substring = @list.length if @list.length > @substring
 end
 
-def handle_non_repeat(letter)
-  @checked[letter] = true
-  @longest = @checked.count if @checked.count > @longest
+def add_to_list(character)
+  @list += character
 end
 
+def repeat?(character)
+  @list.include?(character)
+end
 
-p(length_of_longest_substring("aab"))
+def handle_repeat(character, index)
+  # find index of repeat in list (should just be one) and split at index
+  repeat_index = @list.index(character)
+  p("repeat at #{repeat_index}")
+  p("current index is #{index}")
+  diff = index - repeat_index
+  @substring = diff if diff > @substring
+  i = index + 1
+  p(@list)
+  p(@list[i..])
+  @list = @list[(index + 1)..]
+end
+
+ # When repeat, subtract index of first letter from index of current,
+ # then delete all elements from array before and including first letter array[..index]
+
+p(length_of_longest_substring("abcabcbb"))
+
+# end actions
+# always add new character to list
+# if repeat, reset list to start after first instance of repeat
+# adjust @substring
