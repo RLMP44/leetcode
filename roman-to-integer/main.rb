c34fc6dc819ba7341ae4c0835c2414b7734b4823
Rachael Momber <rlmomber@gmail.com>
2024-03-22 15:38:59 +0900

    refactored

--- roman-to-integer/main.rb
+++ roman-to-integer/main.rb
@@ -1,6 +1,3 @@
-# @param {String} s
-# @return {Integer}
-
 HASH = {
   'I' => 1,
   'V' => 5,
@@ -20,11 +17,7 @@ NEXT = {
 def roman_to_int(s)
   integer = 0
   s.chars.each_with_index do |char, index|
-    if need_reduce?(char, s, index)
-      integer += -HASH[char]
-    else
-      integer += HASH[char]
-    end
+    integer += need_reduce?(char, s, index) ? -HASH[char] : HASH[char]
   end
   integer
 end
@@ -32,10 +25,3 @@ end
 def need_reduce?(char, s, index)
   NEXT.keys.include?(char) && NEXT[char].include?(s[index + 1])
 end
-
-
-# have a string
-# iterate
-# if character is V or X and I is in front, character value - 1
-# if character is L or C and X is in front, character value - 10
-# if character is D or M and C is in front, character value - 100
