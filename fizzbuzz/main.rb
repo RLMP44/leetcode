6a51de0ba401efcf13a2deefaecdeff959cf96e7
Rachael Momber <rlmomber@gmail.com>
2024-03-22 20:10:44 +0900

    finish

--- fizzbuzz/main.rb
+++ fizzbuzz/main.rb
@@ -1,3 +1,17 @@
-def fizz_buzz(num)
+FIZZ = 3
+BUZZ = 5
 
+def fizz_buzz(n)
+  array = []
+  (1..n).to_a.each do |num|
+    array << generate_result(num)
+  end
+  array
+end
+
+def generate_result(num)
+  res = ''
+  res += 'Fizz' if (num % FIZZ).zero?
+  res += 'Buzz' if (num % BUZZ).zero?
+  res == '' ? num.to_s : res
 end
