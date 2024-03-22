fad6c76eb958ec440452f27ce25808f100916d40
Rachael Momber <rlmomber@gmail.com>
2024-03-22 19:10:20 +0900

    refactored

--- pascals-triangle/main.rb
+++ pascals-triangle/main.rb
@@ -1,30 +1,26 @@
-# @param {Integer} num_rows
-# @return {Integer[][]}
-
 def generate(num_rows)
-  rows = []
-  new_row = []
-  initial_num = 1
+  @rows = []
+  @new_row = []
+  @initial_num = 1
   (1..num_rows).each_with_index do |row, index|
-    case index
-    when 0
-      new_row = [initial_num] if index == 0
-    when 1
-      new_row = [initial_num, initial_num] if index == 1
-    else
-      new_row << rows[index - 1][0]
-      (0..index - 2).each do |num|
-        new_row << rows[index - 1][num] + rows[index - 1][num + 1]
-      end
-      new_row << rows[index - 1][-1]
-    end
-    rows << new_row
-    new_row = []
+    @new_row = generate_initial_rows(index)
+    generate_inner_row(index) if @new_row.size >= 2
+    reset_current_row
   end
-  rows
+  @rows
 end
 
-p(generate(1))
-# iterate over row nums
-# add row
-# number = past_row[index] + past_row[index + 1]
+def generate_initial_rows (index)
+  index == 0 ? [@initial_num] : [@initial_num, @initial_num]
+end
+
+def generate_inner_row(index)
+  (0..index - 2).each do |num|
+    @new_row.insert(1, @rows[index - 1][num] + @rows[index - 1][num + 1])
+  end
+end
+
+def reset_current_row
+  @rows << @new_row
+  @new_row = []
+end
