a3ea8f59973b08c2180073ba0195cc96c529c4c1
Rachael Momber <rlmomber@gmail.com>
2024-03-23 17:26:49 +0900

    passing rspec but exceeds time limit

--- climbing-stairs/main.rb
+++ climbing-stairs/main.rb
@@ -1,7 +1,8 @@
 def climb_stairs(n)
+  return 1 if [1, 0].include?(n)
 
+  climb_stairs(n - 1) + climb_stairs(n - 2)
 end
-
 
 # ------------------ TESTS --------------------- #
 p(climb_stairs(2))
@@ -12,3 +13,7 @@ p(climb_stairs(2))
 
 # Each time you can either climb 1 or 2 steps.
 # In how many distinct ways can you climb to the top?
+
+# ----------------- PATTERN -------------------- #
+# 0 and 1 only ever have 1 possible path
+# possible paths will always be the sum of the possible paths of the previous 2 numbers
