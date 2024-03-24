151ff1310b49072a5fd9ea4ae1063b9b45d9afd5
Rachael Momber <rlmomber@gmail.com>
2024-03-24 21:08:56 +0900

    solved and refactored

--- find-duplicate/main.rb
+++ find-duplicate/main.rb
@@ -1,5 +1,13 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_duplicate(nums)
+  @seen = {}
+  search(nums)
+end
 
+def search(nums)
+  for num in nums
+    return num if @seen[num]
+    @seen[num] = true
+  end
 end
