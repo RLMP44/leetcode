ad1123df97e50bc7017a6e60593dd48be3a6cf13
Rachael Momber <rlmomber@gmail.com>
2024-03-27 19:43:19 +0900

    nums 1 and 5 working

--- integer-to-roman/main.rb
+++ integer-to-roman/main.rb
@@ -1,5 +1,51 @@
 # @param {Integer} num
 # @return {String}
-def int_to_roman(num)
+HASH = {
+  1 => 'I',
+  5 => 'V',
+  10 => 'X',
+  50 => 'L',
+  100 => 'C',
+  500 => 'D',
+  1000 => 'M',
+}
 
+MULTIPLIER = {
+  1 => 1,
+  2 => 10,
+  3 => 100,
+  4 => 1000
+}
+
+NEXT = {
+  'I' => ['V', 'X'],
+  'X' => ['L', 'C'],
+  'C' => ['D', 'M']
+}
+
+def int_to_roman(num)
+  string = ''
+  individual = num.to_s.chars
+  individual.each_with_index do |number, index|
+    number = number.to_i
+    if [1, 5].include?(number)
+      string += HASH[number * set_multiplier(individual, index)]
+    elsif [4, 9].include?(number)
+      p('elsif')
+      # string += (HASH.key(number * set_multiplier(individual, index)).to_s)
+    else
+      p('else')
+    end
+  end
+  string
 end
+
+def set_multiplier(individual, index)
+  MULTIPLIER[individual[index..].size]
+end
+
+p(int_to_roman(1111))
+
+# check length of number
+# check if 5 or 1
+# check if 4 or 9
--- integer-to-roman/spec/int_to_roman_spec.rb
+++ integer-to-roman/spec/int_to_roman_spec.rb
@@ -6,4 +6,16 @@ describe '#int_to_roman' do
     expect(int_to_roman(58)).to eq('LVIII')
     expect(int_to_roman(1994)).to eq('MCMXCIV')
   end
+
+  it 'should convert 5 and 1 properly' do
+    expect(int_to_roman(5)).to eq('V')
+    expect(int_to_roman(55)).to eq('LV')
+    expect(int_to_roman(1111)).to eq('MCXI')
+  end
+
+  it 'should convert 4 and 9 properly' do
+    expect(int_to_roman(4)).to eq('IV')
+    expect(int_to_roman(49)).to eq('XLIX')
+    expect(int_to_roman(944)).to eq('CMXLIV')
+  end
 end
