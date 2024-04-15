e80b658732351f14d8b88464075d2702c7553600
Rachael Momber <rlmomber@gmail.com>
2024-04-15 20:46:14 +0900

    passing initial rspec tests

--- majority_element/main.rb
+++ majority_element/main.rb
@@ -1,3 +1,7 @@
 def majority_element(nums)
-
+  hash = {}
+  nums.each do |num|
+    hash[num] = nums.count(num) unless hash[num]
+  end
+  hash.find { |key, value| return key if value > (nums.length / 2)}
 end
