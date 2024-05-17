4bc461d15ecedbcc41658a1ef3e5dc65391aafb6
Rachael Momber <rlmomber@gmail.com>
2024-05-17 19:33:02 +0900

    can find index of existing number

--- search-insert-position/main.rb
+++ search-insert-position/main.rb
@@ -1,3 +1,9 @@
 def search_insert(nums, target)
+  nums.include?(target) ? nums.index(target) : insert_index(nums, target)
+end
 
+def insert_index(nums, target)
+  nums.each_with_index do |num, index|
+    return (index + 1) if target > num && target < nums[index + 1]
+  end
 end
