615291bf98b9c2a7be73f67b7bc9500942782699
Rachael Momber <rlmomber@gmail.com>
2024-05-17 19:39:00 +0900

    add new rspec test

--- search-insert-position/spec/search_insert_spec.rb
+++ search-insert-position/spec/search_insert_spec.rb
@@ -8,5 +8,6 @@ describe '#search_insert' do
   it 'should return the index where an unfound number should go' do
     expect(search_insert([1,3,5,6], 2)).to eq(1)
     expect(search_insert([1,3,5,6], 7)).to eq(4)
+    expect(search_insert([1,3,5,6], 0)).to eq(0)
   end
 end
