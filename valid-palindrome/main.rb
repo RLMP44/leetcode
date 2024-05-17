872f24c71b082066205f8603516e3bf5b05d42db
Rachael Momber <rlmomber@gmail.com>
2024-05-17 19:11:44 +0900

    passing tests

--- valid-palindrome/main.rb
+++ valid-palindrome/main.rb
@@ -1,3 +1,13 @@
 def is_palindrome(s)
-
+  formatted_string = reformat(s)
+  formatted_string == formatted_string.reverse
 end
+
+def reformat(string)
+  string.downcase.strip.gsub(/[^a-z0-9]/i, '')
+end
+
+
+puts(is_palindrome("A man, a plan, a canal: Panama"))
+puts(is_palindrome("race a car"))
+puts(is_palindrome(" "))
