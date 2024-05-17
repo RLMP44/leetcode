e11f34346122985a0bb7405e3fe8c9ebe7191a35
Rachael Momber <rlmomber@gmail.com>
2024-05-17 19:27:39 +0900

    install rspec and setup initial tests

---
+++ search-insert-position/spec/search_insert_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../main'
+
+describe '#search_insert' do
+  it 'should return the index of a found number' do
+    expect(search_insert([1,3,5,6], 5)).to eq(2)
+  end
+
+  it 'should return the index where an unfound number should go' do
+    expect(search_insert([1,3,5,6], 2)).to eq(1)
+    expect(search_insert([1,3,5,6], 7)).to eq(4)
+  end
+end
