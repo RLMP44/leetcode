b99c9dc57a0a9c6f70870064712728c82e87cd44
Rachael Momber <rlmomber@gmail.com>
2024-05-17 19:38:12 +0900

    passing initial tests

--- search-insert-position/main.rb
+++ search-insert-position/main.rb
@@ -4,6 +4,14 @@ end
 
 def insert_index(nums, target)
   nums.each_with_index do |num, index|
-    return (index + 1) if target > num && target < nums[index + 1]
+    if nums[index + 1]
+      return (index + 1) if target > num && target < nums[index + 1]
+    else
+      return index + 1
+    end
   end
 end
+
+puts(search_insert([1,3,5,6], 5))
+puts(search_insert([1,3,5,6], 2))
+puts(search_insert([1,3,5,6], 7))
