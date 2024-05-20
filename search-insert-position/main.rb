3b3e6252111df4b7092b0320ccdb8324e5eb30ef
Rachael Momber <rlmomber@gmail.com>
2024-05-20 22:52:02 +0900

    solved different way

--- search-insert-position/main.rb
+++ search-insert-position/main.rb
@@ -3,15 +3,11 @@ def search_insert(nums, target)
 end
 
 def insert_index(nums, target)
-  nums.each_with_index do |num, index|
-    if nums[index + 1] && target > num && target < nums[index + 1]
-      return index + 1
-    elsif target < nums[0]
-      return 0
-    else
-      return nums.count
-    end
+  point = nums.select do |num|
+    num < target
   end
+  index = nums.index(point[-1])
+  index ? index + 1 : 0
 end
 
 puts(search_insert([1,3,5,6], 5))
