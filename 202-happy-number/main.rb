f2afcaf5b7f65d4a367d9d26c2a8a4e19c6c5808
Rachael Momber <rlmomber@gmail.com>
2024-05-25 19:32:42 -0400

    refactored

--- 202-happy-number/main.rb
+++ 202-happy-number/main.rb
@@ -2,31 +2,16 @@ def is_happy(n)
   seen = []
   num = n
   loop do
-    new_num = calculate(num)
-    return true if new_num == 1
-    return false if seen.include?(new_num)
+    num = calculate(num)
+    return true if num == 1
+    return false if seen.include?(num)
 
-    seen << new_num
-    num = new_num
+    seen << num
   end
-  new_num == 1
+  num == 1
 end
 
 def calculate(n)
   squares = n.digits.map { |num| num * num }
   squares.sum
 end
-
-p is_happy(19)
-
-# def convert(digits)
-#   digits.map do |num|
-#     num = num.to_i
-#     num * num
-#   end
-# end
-
-# def adjust_starters(new_number)
-#   @number = new_number
-#   @loop += 1
-# end
