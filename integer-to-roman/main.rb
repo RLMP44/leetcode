4b75eab47734c13d10880167013138df98fff41b
Rachael Momber <rlmomber@gmail.com>
2024-05-26 00:04:01 -0400

    update: refactored with new methods

--- integer-to-roman/main.rb
+++ integer-to-roman/main.rb
@@ -27,29 +27,57 @@ PREVIOUS = {
 }
 
 def int_to_roman(num)
-  string = ''
-  split_nums = num.to_s.chars
-  split_nums.each_with_index do |number, index|
-    number = number.to_i
-    if [1, 5].include?(number)
-      string += HASH[number * set_multiplier(split_nums, index)]
-    elsif [4, 9].include?(number)
-      string += PREVIOUS[HASH[((number + 1) * set_multiplier(split_nums, index))]]
-      string += HASH[((number + 1) * set_multiplier(split_nums, index))]
-    elsif number < 5
-      number.times { string += HASH[set_multiplier(split_nums, index)] }
-    elsif number > 5
-      string += HASH[5 * set_multiplier(split_nums, index)]
-      (number - 5).times do
-        string += HASH[set_multiplier(split_nums, index)]
-      end
-    end
-  end
-  string
+  @string = ''
+  calculate(num.to_s.chars)
+  @string
 end
 
 def set_multiplier(split_nums, index)
   MULTIPLIER[split_nums[index..].size]
+end
+
+def calculate(split_nums)
+  split_nums.each_with_index do |number, index|
+    number = number.to_i
+    @base_number = set_base_number(number)
+    if [1, 5].include?(number)
+      @string += first_numeral(split_nums, index)
+    elsif [4, 9].include?(number)
+      @string += PREVIOUS[first_numeral(split_nums, index)]
+      @string += first_numeral(split_nums, index)
+    elsif number < 5
+      number.times { @string += repeat_numerals(split_nums, index) }
+    elsif number > 5
+      @string += first_numeral(split_nums, index)
+      (number - 5).times do
+        @string += repeat_numerals(split_nums, index)
+      end
+    end
+  end
+end
+
+def repeat_numerals(split_nums, index)
+  HASH[set_multiplier(split_nums, index)]
+end
+
+def first_numeral(split_nums, index)
+  HASH[@base_number * set_multiplier(split_nums, index)]
+end
+
+def calculated(split_nums, index)
+  HASH[(@adjusted_base * set_multiplier(split_nums, index))]
+end
+
+def set_base_number(number)
+  if [4, 9].include?(number)
+    number + 1
+  elsif [1, 5].include?(number)
+    number
+  elsif number > 5
+    5
+  else
+    1
+  end
 end
 
 p(int_to_roman(483))
